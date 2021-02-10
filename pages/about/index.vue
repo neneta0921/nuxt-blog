43e42904925c88faa9f73ee58893409d54c49eaf
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2021-02-10 15:10:09 +0900

    creating the Main sections

---
+++ pages/about/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="about-page">
+    <h1>This is my absolutely fantastic "About Me" Page!</h1>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque veniam,
+      esse iure iusto nemo quasi hic atque dicta ipsa cum ducimus quod nulla
+      nesciunt? Provident ullam eius architecto recusandae soluta.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque veniam,
+      esse iure iusto nemo quasi hic atque dicta ipsa cum ducimus quod nulla
+      nesciunt? Provident ullam eius architecto recusandae soluta.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque veniam,
+      esse iure iusto nemo quasi hic atque dicta ipsa cum ducimus quod nulla
+      nesciunt? Provident ullam eius architecto recusandae soluta.
+    </p>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque veniam,
+      esse iure iusto nemo quasi hic atque dicta ipsa cum ducimus quod nulla
+      nesciunt? Provident ullam eius architecto recusandae soluta.
+    </p>
+  </div>
+</template>
+
+<style scoped>
+.about-page {
+  text-align: center;
+  padding: 30px;
+}
+</style>
