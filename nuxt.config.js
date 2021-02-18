42c9166f0d8a4c8b51e06997e1a50fa4f4f7f28d
neneta0921 <65237907+neneta0921@users.noreply.github.com>
2021-02-18 12:54:51 +0900

    delete comment

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,8 +2,6 @@ const bodyParser = require("body-parser");
 const axios = require("axios");
 
 module.exports = {
-  // ssr: false,
-
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: "WD Blog",
